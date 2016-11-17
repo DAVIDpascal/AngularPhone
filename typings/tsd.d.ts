4375b4fd604ac358d50fbda35b281a5c4a1b6873
Pascal DAVID <P093388@activ.aviva.corp>
2016-11-17 16:56:29 +0100

    my first commit

---
+++ typings/tsd.d.ts
@@ -0,0 +1,6 @@
+
+/// <reference path="jasmine/jasmine.d.ts" />
+/// <reference path="angular-protractor/angular-protractor.d.ts" />
+/// <reference path="selenium-webdriver/selenium-webdriver.d.ts" />
+/// <reference path="angularjs/angular.d.ts" />
+/// <reference path="jquery/jquery.d.ts" />
